3bd825f59f73355b97fce7bc3bd7b16d5d764edd
Sneakilli <77079340+Sneakilli@users.noreply.github.com>
2021-02-18 00:16:48 +0000

    Set ShouldTweet to True by default; more logging

--- SeedPreviewer.py
+++ SeedPreviewer.py
@@ -6,27 +6,28 @@ import time
 
 api = tweepyauth.auto_authenticate()
 
-basepath = "G:/MultiMC/instances/1.16.5/.minecraft/screenshots/"
-TimeFile = "LastTweetTime.txt"
-PostingHour = 17
+basepath = "G:/MultiMC/instances/1.16.5/.minecraft/screenshots/" #Folder containing seed screenshots
+TimeFile = "LastTweetTime.txt" #File name for keeping track of when to tweet
+mcVersion = "1.16.5" #Tweets this as the minecraft version
+PostingHour = 17 #When should the bot tweet each day
 
-ShouldTweet = False
+ShouldTweet = True #Whether the bot should tweet or remove directories. Good for debugging
 
 def DoTweet():
+    print ("Making a tweet")
     AllSeeds = os.listdir(basepath)
     Seed = AllSeeds[0]
     TodaySeed = basepath + Seed
     ImageIDList = []
     if (ShouldTweet):
+        print ("Uploading images")
         for x in os.listdir(TodaySeed):
             ImageIDList.append(api.media_upload(TodaySeed + "/" + x).media_id)
-
-        TextForTweet = "Seed:" + Seed + "\nVersion: 1.16.5" #Space is put into directory by AHK
+        
+        TextForTweet = "Seed:" + Seed + "\nVersion: " + mcVersion #Space is put into directory by AHK
         tweet = api.update_status(status=TextForTweet,media_ids=ImageIDList)
         DeleteDirectory(TodaySeed)
     print ("tweeted seed" + Seed)
-    
-    
     
     return datetime.now().day
 
